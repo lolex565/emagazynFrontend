1c262a0f22089903212b204446350c21a4838468
Miłosz Bedryło <milosz.bedrylo@gmail.com>
2020-03-25 20:47:17 +0100

    initial commit to emagazyn frontend repo merely working vanilla js login script that shows the test secret when paired with the backend

---
+++ js/jwt-vanilla.js
@@ -0,0 +1,47 @@
+    // make the request to the login endpoint
+    function getToken() {
+      var loginUrl = "http://localhost:3000/login";
+      var xhr = new XMLHttpRequest();
+      var userElement = document.getElementById('username');
+      var passwordElement = document.getElementById('password');
+      var user = userElement.value;
+      var password = passwordElement.value;
+    
+      xhr.open('POST', loginUrl, true);
+      xhr.setRequestHeader('Access-Control-Allow-Origin','*');
+      xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+      xhr.addEventListener('load', function() {
+        var responseObject = JSON.parse(this.response);
+        console.log(responseObject);
+        if (responseObject.token) {
+          localStorage.setItem('token', responseObject.token);
+          getSecret();
+        } else {
+          localStorage.setItem('token', "No token received");
+        }
+      });
+    
+      var sendObject = JSON.stringify({password: password, username: user});
+    
+      console.log('going to send', sendObject);
+    
+      xhr.send(sendObject);
+    }
+  
+    // make the request to the secret API endpoint
+  function getSecret() {
+  
+      var url = "http://localhost:3000/";
+      var xhr = new XMLHttpRequest();
+      var tokenElement = localStorage.getItem('token');
+      var resultElement = document.getElementById('result');
+      xhr.open('GET', url, true);
+      xhr.setRequestHeader("Authorization", "Bearer " + tokenElement);
+      xhr.addEventListener('load', function() {
+        var responseObject = JSON.parse(this.response);
+        console.log(responseObject);
+        resultElement.innerHTML = responseObject.message;
+      });
+    
+      xhr.send(null);
+    }
