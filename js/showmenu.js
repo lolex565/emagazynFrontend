56196d7aac73d6239a27a0bf00e39a1e210472a4
Miłosz Bedryło <milosz.bedrylo@gmail.com>
2020-05-11 13:18:38 +0200

    added logout on landing

--- js/showmenu.js
+++ js/showmenu.js
@@ -3,9 +3,15 @@ function showMenu() {
     getUserRole();
     var resultElement = document.getElementById('menu');
     if (localStorage.role.includes("admin")) {
-        resultElement.innerHTML += '<input type=\"button\" value=\"admin panel\" onclick=\"redirectToAdmin()\"></input>';
+        resultElement.innerHTML += '<input type=\"button\" value=\"admin panel\" onclick=\"redirectToAdmin()\" />';
     }
-        resultElement.innerHTML += '<input type=\"button\" value=\"magazyn\" onclick=\"redirectToStore()\"></input>';
-        resultElement.innerHTML += '<input type=\"button\" value=\"archiwum\" onclick=\"redirectToArchive()\"></input>';
-        resultElement.innerHTML += '<input type=\"button\" value=\"biblioteczka\" onclick=\"redirectToLibrary()\"></input>';
+    if (localStorage.role.includes("storage")) {
+        resultElement.innerHTML += '<input type=\"button\" value=\"magazyn\" onclick=\"redirectToStore()\" />';
+    }
+    if (localStorage.role.includes("archive")) {
+        resultElement.innerHTML += '<input type=\"button\" value=\"archiwum\" onclick=\"redirectToArchive()\" />';
+    }
+    if (localStorage.role.includes("library")) {
+        resultElement.innerHTML += '<input type=\"button\" value=\"biblioteczka\" onclick=\"redirectToLibrary()\" />';
+    }
 }
